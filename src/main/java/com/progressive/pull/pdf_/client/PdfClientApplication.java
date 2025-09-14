8d2b47272881e157bd826d7095b2c644a9722782
John Smith <progressivepull@gmail.com>
2025-09-13 22:29:08 -0500

    init commit

---
+++ src/main/java/com/progressive/pull/pdf_/client/PdfClientApplication.java
@@ -0,0 +1,13 @@
+package com.progressive.pull.pdf_.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progressive/pull/pdf_/client/PdfClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progressive.pull.pdf_.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
